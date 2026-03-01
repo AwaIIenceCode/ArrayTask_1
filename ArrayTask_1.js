8b1d7d14f7844147761dc5f25225b0179e01ea41
AwallenceCode <awallencecode@gmail.com>
2026-03-01 22:02:33 +0200

    Initialize ArrayTask_1.js with function stubs and update .gitignore to include .DS_Store

---
+++ ArrayTask_1.js
@@ -0,0 +1,24 @@
+const prompt = require("prompt-sync")();
+
+// function for generating numbers in an array
+function createArray() 
+{
+
+}
+
+// function for writing numbers in console
+function writingArray()
+{
+
+}
+
+// function for finding the sum in an array 
+function findSumNumbersArray()
+{
+
+}
+
+function main() 
+{
+    const userArr = [10];
+}
